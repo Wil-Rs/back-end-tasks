d8b162ab420da518570415f335e443a6ddc30b19
Wil-Rs <43270145+Wil-Rs@users.noreply.github.com>
2021-02-03 01:58:56 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
 
     client: 'postgresql',
-    connection: `postgres://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.HOST_DB}:${process.env.PORT_DB}/${process.env.DATABASE_BASE}?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory`,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
